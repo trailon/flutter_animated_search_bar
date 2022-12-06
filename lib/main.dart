1ec650e48d3a9be4d382073bc34390173f8749e7
trailon <cetinelv@gmail.com>
2022-12-06 17:38:15 +0300

    A better half complete version of anim search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,327 @@
-// i dont know why the Icons.mic show the Tick Icon... cant do anything about it :(
-
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class AnimSearchBar extends StatefulWidget {
-  const AnimSearchBar({super.key, required this.controller});
-  final TextEditingController? controller;
+extension on Color {
+  double contrastWithColor(Color other) {
+    final thisBrightness = computeLuminance();
+    final otherBrightness = other.computeLuminance();
 
-  @override
-  AnimSearchBarState createState() => AnimSearchBarState();
+    final brightest = math.max(thisBrightness, otherBrightness);
+    final darkest = math.min(thisBrightness, otherBrightness);
+
+    return (brightest + 0.05) / (darkest + 0.05);
+  }
+
+  bool hasGoodContrastWithColor(Color other) {
+    return contrastWithColor(other) > 4.5;
+  }
+
+  Color get contrastingColor {
+    if (hasGoodContrastWithColor(Colors.white)) {
+      return Colors.white;
+    } else {
+      return Colors.black;
+    }
+  }
 }
 
-int toggle = 0;
+class AnimSearchBarTrial extends StatefulWidget {
+  ///  width - double ,isRequired : Yes
+  ///  textController - TextEditingController  ,isRequired : Yes
+  ///  onSuffixTap - Function, isRequired : Yes
+  ///  rtl - Boolean, isRequired : No
+  ///  autoFocus - Boolean, isRequired : No
+  ///  style - TextStyle, isRequired : No
+  ///  closeSearchOnSuffixTap - bool , isRequired : No
+  ///  suffixIcon - Icon ,isRequired :  No
+  ///  prefixIcon - Icon  ,isRequired : No
+  ///  animationDurationInMilli -  int ,isRequired : No
+  ///  helpText - String ,isRequired :  No
+  /// inputFormatters - TextInputFormatter, Required - No
+  final double width;
+  final TextEditingController controller;
+  final Icon? suffixIcon;
+  final Icon? prefixIcon;
+  final String helpText;
+  final int animationDurationInMilli;
+  final onSuffixTap;
+  final bool rtl;
+  final bool autoFocus;
+  final TextStyle? style;
+  final bool closeSearchOnSuffixTap;
+  final Color? color;
+  final InputDecoration? textFieldDecoration;
+  final List<TextInputFormatter>? inputFormatters;
+  final Function(String)? onChanged;
 
-class AnimSearchBarState extends State<AnimSearchBar>
+  const AnimSearchBarTrial({
+    Key? key,
+
+    /// The width cannot be null
+    required this.width,
+
+    /// The textController cannot be null
+    required this.controller,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.helpText = "Search...",
+    this.textFieldDecoration,
+
+    /// choose your custom color
+    this.color = Colors.white,
+
+    /// The onSuffixTap cannot be null
+    required this.onSuffixTap,
+    this.animationDurationInMilli = 375,
+
+    /// make the search bar to open from right to left
+    this.rtl = false,
+    this.onChanged,
+
+    /// make the keyboard to show automatically when the searchbar is expanded
+    this.autoFocus = false,
+
+    /// TextStyle of the contents inside the searchbar
+    this.style,
+
+    /// close the search on suffix tap
+    this.closeSearchOnSuffixTap = false,
+
+    /// can add list of inputformatters to control the input
+    this.inputFormatters,
+  }) : super(key: key);
+
+  @override
+  _AnimSearchBarTrialState createState() => _AnimSearchBarTrialState();
+}
+
+///toggle - 0 => false or closed
+///toggle 1 => true or open
+bool toggle = false;
+
+class _AnimSearchBarTrialState extends State<AnimSearchBarTrial>
     with SingleTickerProviderStateMixin {
-  TextEditingController? get _textEditingController => widget.controller;
+  ///initializing the AnimationController
   late AnimationController _con;
+  FocusNode focusNode = FocusNode();
+
   @override
   void initState() {
     super.initState();
+
+    ///Initializing the animationController which is responsible for the expanding and shrinking of the search bar
     _con = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 375),
+
+      /// animationDurationInMilli is optional, the default value is 375
+      duration: Duration(milliseconds: widget.animationDurationInMilli),
     );
+  }
+
+  unfocusKeyboard() {
+    final FocusScopeNode currentScope = FocusScope.of(context);
+    if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
+      FocusManager.instance.primaryFocus?.unfocus();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 375),
-      height: 48.0,
-      width: (toggle == 0) ? 48.0 : 250.0,
-      curve: Curves.easeOut,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30.0),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black26,
-            spreadRadius: -10.0,
-            blurRadius: 10.0,
-            offset: Offset(0.0, 10.0),
-          ),
-        ],
-      ),
-      child: Stack(
-        children: [
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 375),
-            top: 6.0,
-            right: 7.0,
-            curve: Curves.easeOut,
-            child: AnimatedOpacity(
-              opacity: (toggle == 0) ? 0.0 : 1.0,
-              duration: const Duration(milliseconds: 200),
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: const Color(0xffF2F3F7),
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: AnimatedBuilder(
-                  builder: (context, widget) {
-                    return Transform.rotate(
-                      angle: _con.value * 2.0 * pi,
-                      child: widget,
-                    );
-                  },
-                  animation: _con,
-                  child: const Icon(
-                    Icons.mic,
-                    size: 20.0,
+    return Container(
+      height: 100.0,
+
+      ///if the rtl is true, search bar will be from right to left
+      alignment:
+          widget.rtl ? Alignment.centerRight : const Alignment(-1.0, 0.0),
+
+      ///Using Animated container to expand and shrink the widget
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: widget.animationDurationInMilli),
+        height: 48.0,
+        width: (!toggle) ? 48.0 : widget.width,
+        curve: Curves.easeOut,
+        decoration: BoxDecoration(
+          /// can add custom color or the color will be white
+          color: widget.color,
+          borderRadius: BorderRadius.circular(30.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              spreadRadius: -10.0,
+              blurRadius: 10.0,
+              offset: Offset(0.0, 10.0),
+            ),
+          ],
+        ),
+        child: Stack(
+          children: [
+            ///Using Animated Positioned widget to expand and shrink the widget
+            AnimatedPositioned(
+              duration: Duration(milliseconds: widget.animationDurationInMilli),
+              top: 6.0,
+              right: 7.0,
+              curve: Curves.easeOut,
+              child: AnimatedOpacity(
+                opacity: (!toggle) ? 0.0 : 1.0,
+                duration: const Duration(milliseconds: 200),
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    /// can add custom color or the color will be white
+                    color: widget.color,
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: AnimatedBuilder(
+                    builder: (context, widget) {
+                      ///Using Transform.rotate to rotate the suffix icon when it gets expanded
+                      return Transform.rotate(
+                        angle: _con.value * 2.0 * math.pi,
+                        child: widget,
+                      );
+                    },
+                    animation: _con,
+                    child: GestureDetector(
+                      onTap: () {
+                        try {
+                          ///trying to execute the onSuffixTap function
+                          widget.onSuffixTap();
+
+                          ///closeSearchOnSuffixTap will execute if it's true
+                          if (widget.closeSearchOnSuffixTap) {
+                            unfocusKeyboard();
+                            setState(() {
+                              toggle = false;
+                            });
+                          }
+                        } catch (e) {
+                          ///print the error if the try block fails
+                          debugPrint(e.toString());
+                        }
+                      },
+
+                      ///suffixIcon is of type Icon
+                      child: widget.suffixIcon ??
+                          const Icon(
+                            Icons.close,
+                            size: 20.0,
+                          ),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 375),
-            left: (toggle == 0) ? 20.0 : 40.0,
-            curve: Curves.easeOut,
-            top: 11.0,
-            child: AnimatedOpacity(
-              opacity: (toggle == 0) ? 0.0 : 1.0,
-              duration: const Duration(milliseconds: 200),
-              child: SizedBox(
-                height: 23.0,
-                width: 180.0,
-                child: TextField(
-                  controller: _textEditingController,
-                  cursorRadius: const Radius.circular(10.0),
-                  cursorWidth: 2.0,
-                  cursorColor: Colors.black,
-                  decoration: InputDecoration(
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelText: 'Search...',
-                    labelStyle: const TextStyle(
-                      color: Color(0xff5B5B5B),
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    alignLabelWithHint: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      borderSide: BorderSide.none,
-                    ),
+            AnimatedPositioned(
+              duration: Duration(milliseconds: widget.animationDurationInMilli),
+              left: (!toggle) ? 20.0 : 40.0,
+              curve: Curves.easeOut,
+              top: 11.0,
+
+              ///Using Animated opacity to change the opacity of th textField while expanding
+              child: AnimatedOpacity(
+                opacity: (!toggle) ? 0.0 : 1.0,
+                duration: const Duration(milliseconds: 200),
+                child: Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  alignment: Alignment.topCenter,
+                  width: widget.width / 1.7,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.transparent)),
+                  child: TextField(
+                    ///Text Controller. you can manipulate the text inside this textField by calling this controller.
+                    controller: widget.controller,
+                    inputFormatters: widget.inputFormatters,
+                    focusNode: focusNode,
+                    cursorRadius: const Radius.circular(10.0),
+                    cursorWidth: 1.5,
+                    onChanged: widget.onChanged,
+                    onEditingComplete: () {
+                      /// on editing complete the keyboard will be closed and the search bar will be closed
+                      unfocusKeyboard();
+                      setState(() {
+                        toggle = false;
+                      });
+                    },
+
+                    ///style is of type TextStyle, the default is just a color black
+                    style: widget.style ??
+                        TextStyle(color: widget.color?.contrastingColor),
+                    cursorColor: widget.color?.contrastingColor,
+                    decoration: widget.textFieldDecoration ??
+                        InputDecoration(
+                          fillColor: Colors.transparent,
+                          contentPadding:
+                              const EdgeInsets.only(bottom: 5, left: 6.5),
+                          isDense: true,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          labelText: widget.helpText,
+                          labelStyle: const TextStyle(
+                            color: Color(0xff5B5B5B),
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          alignLabelWithHint: true,
+                          /*  border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            borderSide: BorderSide.none,
+                          ), */
+                        ),
                   ),
                 ),
               ),
             ),
-          ),
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 375),
-            left: 4,
-            top: 4,
-            child: Material(
-              color: Colors.white,
+
+            ///Using material widget here to get the ripple effect on the prefix icon
+            Material(
+              /// can add custom color or the color will be white
+              color: widget.color,
               borderRadius: BorderRadius.circular(30.0),
               child: IconButton(
                 splashRadius: 19.0,
-                icon: const Icon(
-                  Icons.search,
-                  size: 24.0,
-                ),
+
+                ///if toggle is 1, which means it's open. so show the back icon, which will close it.
+                ///if the toggle is 0, which means it's closed, so tapping on it will expand the widget.
+                ///prefixIcon is of type Icon
+                icon: widget.prefixIcon != null
+                    ? toggle
+                        ? Icon((widget.rtl)
+                            ? Icons.arrow_forward_ios
+                            : Icons.arrow_back_ios)
+                        : widget.prefixIcon!
+                    : Icon(
+                        toggle
+                            ? (widget.rtl
+                                ? Icons.arrow_forward_ios
+                                : Icons.arrow_back_ios)
+                            : Icons.search,
+                        size: 20.0,
+                      ),
                 onPressed: () {
                   setState(
                     () {
-                      if (toggle == 0) {
-                        toggle = 1;
+                      ///if the search bar is closed
+                      if (!toggle) {
+                        toggle = true;
+                        setState(() {
+                          ///if the autoFocus is true, the keyboard will pop open, automatically
+                          if (widget.autoFocus) {
+                            FocusScope.of(context).requestFocus(focusNode);
+                          }
+                        });
+
+                        ///forward == expand
                         _con.forward();
                       } else {
-                        toggle = 0;
-                        _textEditingController?.clear();
+                        ///if the search bar is expanded
+                        toggle = false;
+
+                        ///if the autoFocus is true, the keyboard will close, automatically
+                        setState(() {
+                          if (widget.autoFocus) unfocusKeyboard();
+                        });
+
+                        ///reverse == close
                         _con.reverse();
                       }
                     },
@@ -141,8 +329,8 @@ class AnimSearchBarState extends State<AnimSearchBar>
                 },
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
